9464d544c0ad17ab7d911959f66197b2fe3bb643
Barbara <varka.sinitsckaya@yandex.ru>
2021-04-07 00:04:09 +0300

    brr

--- C_1_base/C_5_base/Form1.Designer.cs
+++ C_1_base/C_5_base/Form1.Designer.cs
@@ -29,7 +29,166 @@ namespace C_5_base
         /// </summary>
         private void InitializeComponent()
         {
+<<<<<<< HEAD
             this.components = new System.ComponentModel.Container();
+||||||| constructed merge base
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(286, 18);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Up";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Current directory:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(16, 131);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(766, 307);
+            this.treeView1.TabIndex = 2;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(120, 23);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Local disc:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 20);
+            this.label3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(161, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "choose directory";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+=======
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(286, 18);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Up";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Current directory:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(16, 131);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(766, 307);
+            this.treeView1.TabIndex = 2;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(120, 23);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Local disc:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 20);
+            this.label3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(161, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "choose directory";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+>>>>>>> all works
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Text = "Form1";
--- C_1_base/C_5_base/Form1.cs
+++ C_1_base/C_5_base/Form1.cs
@@ -15,6 +15,170 @@ namespace C_5_base
         public Form1()
         {
             InitializeComponent();
+<<<<<<< HEAD
+||||||| constructed merge base
+            InitializeComboBox();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { // кнопка Up
+            string bla = label4.Text;
+            int index = bla.LastIndexOf('\\');
+            if ((index != -1) && (bla != comboBox1.Text))
+            {
+                label4.Text = bla.Substring(0, index); 
+                if (label4.Text.LastIndexOf('\\') == -1)
+                { // попадаем сюда только когда вернулись кнопкой Up в родительский каталог
+                    label4.Text += '\\';
+                    //button1.Enabled = false;
+                }
+                List<string> directory_names = Directory.GetFileSystemEntries(label4.Text).ToList();
+                treeView1.Nodes.Clear();
+                foreach (string d in directory_names)
+                    treeView1.Nodes.Add(d);
+                
+            }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            List<string> filenames;
+            filenames = Directory.GetDirectories(comboBox1.SelectedItem.ToString()).ToList();
+            foreach (string f in filenames)
+                treeView1.Nodes.Add(f);
+            filenames = Directory.GetFiles(comboBox1.SelectedItem.ToString()).ToList();
+            foreach (string f in filenames)
+                treeView1.Nodes.Add(f);
+            label4.Text = comboBox1.SelectedItem.ToString();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string bla = treeView1.SelectedNode.Text;
+            label4.Text = bla;
+            List<string> directory_names = new List<string>();
+            if (Directory.Exists(bla)) // выбрали папку
+                directory_names = Directory.GetFileSystemEntries(bla).ToList();
+            else // выбрали файл
+                Process.Start(bla);
+            treeView1.Nodes.Clear();
+            foreach (string d in directory_names)
+                treeView1.Nodes.Add(d);
+            button1.Enabled = true;
+        }
+
+        private void InitializeComboBox()
+        {
+            button1.Enabled = false;
+            // начинаем с выбора локального диска в комбобоксе
+            // для этого добавляем локальные диски в комбобокс
+            DriveInfo[] local_discs = DriveInfo.GetDrives();
+            foreach (DriveInfo disc in local_discs)
+                comboBox1.Items.Add(disc.Name);
+            // добавили локальные диски
+            //button1.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+=======
+            InitializeComboBox();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { // кнопка Up
+            string bla = label4.Text;
+            int index = bla.LastIndexOf('\\');
+            if ((index != -1) && (bla != comboBox1.Text))
+            {
+                label4.Text = bla.Substring(0, index); 
+                if (label4.Text.LastIndexOf('\\') == -1)
+                { // попадаем сюда только когда вернулись кнопкой Up в родительский каталог
+                    label4.Text += '\\';
+                    button1.Enabled = false;
+                }
+                List<string> directory_names = Directory.GetFileSystemEntries(label4.Text).ToList();
+                treeView1.Nodes.Clear();
+                foreach (string d in directory_names)
+                    treeView1.Nodes.Add(d);
+                
+            }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            List<string> filenames;
+            filenames = Directory.GetFileSystemEntries(comboBox1.SelectedItem.ToString()).ToList();
+            treeView1.Nodes.Clear();
+            foreach (string f in filenames)
+                treeView1.Nodes.Add(f);
+            label4.Text = comboBox1.SelectedItem.ToString();
+        }
+
+        bool treeView1Reset = true;
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (!treeView1Reset)
+            {
+                string bla = treeView1.SelectedNode.Text;
+                label4.Text = bla;
+                List<string> directory_names = new List<string>();
+                if (Directory.Exists(bla)) // выбрали папку
+                    directory_names = Directory.GetFileSystemEntries(bla).ToList();
+                else // выбрали файл
+                    Process.Start(bla);
+                treeView1.Nodes.Clear();
+                foreach (string d in directory_names)
+                    treeView1.Nodes.Add(d);
+                button1.Enabled = true;
+                treeView1Reset = true;
+            }
+        }
+
+        private void InitializeComboBox()
+        {
+            button1.Enabled = false;
+            // начинаем с выбора локального диска в комбобоксе
+            // для этого добавляем локальные диски в комбобокс
+            DriveInfo[] local_discs = DriveInfo.GetDrives();
+            foreach (DriveInfo disc in local_discs)
+                comboBox1.Items.Add(disc.Name);
+            // добавили локальные диски
+            //button1.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+>>>>>>> all works
+        }
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            treeView1Reset = false;
         }
     }
 }
